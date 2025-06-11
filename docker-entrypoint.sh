f59a48066f935926bca08c8c1e0e0fb73baa9b81
SupRemaZie <Damien.Porchet.Etu@univ-lemans.fr>
2025-06-11 09:58:08 +0200

    fix docker bip

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
-
 set -e
 
-echo "🕒 Attente de la base de données PostgreSQL..."
-npx wait-on tcp:db:5432 || echo "Skip wait if using Neon"
-
-echo "✅ Base de données disponible"
-
-# Génération du client Prisma
-echo "⚙️ Génération du client Prisma..."
-npx prisma generate
-
-# Déploiement des migrations (utile si tu veux garder ta base à jour automatiquement)
-echo "🧩 Application des migrations..."
+echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
-# Exécution du seed
-echo "🌱 Insertion des données de seed..."
-npx prisma db seed
-
-# Lancement de l'application
-echo "🚀 Lancement de l'application..."
+echo "Starting Next.js app..."
 exec "$@"
